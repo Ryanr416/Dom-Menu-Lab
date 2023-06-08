0dd79beb5781295891595bc1c2f535020f64f7c4
ryanr416 <ryanrichardson0993@gmail.com>
2023-06-08 16:59:54 -0400

    dom-menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 console.log('hello')
+
+// Menu data structure
+const menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+];
+
+const mainEl = document.querySelector('main');
+mainEl.classList.add('flex-ctr');
+const topMenuEl = document.querySelector('top-menu');
+topMenuEl.classList.add('flex-around');
+
+
+
